9fab333846aa7ebd58f50f39a9a37506a46b45e2
Khalid.elgazzar <Khalid.elgazzar@gmail.com>
2020-01-09 21:56:40 +0200

    Re-solved the sample or actually re-copied it :) but with more understanding now

--- preprocess_inputs.py
+++ preprocess_inputs.py
@@ -11,7 +11,21 @@ def pose_estimation(input_image):
     preprocessed_image = np.copy(input_image)
 
     # TODO: Preprocess the image for the pose estimation model
-    
+    '''
+    Inputs
+    name: "input" , shape: [1x3x256x456] - An input image in the format [BxCxHxW], where:
+    B - batch size
+    C - number of channels
+    H - image height
+    W - image width. Expected color order is BGR.
+    '''
+
+    height = 256 
+    width = 456
+
+    preprocessed_image = cv2.resize(input_image, (width, height))
+    preprocessed_image = preprocessed_image.transpose((2,0,1))
+    preprocessed_image = preprocessed_image.reshape(1, 3, height, width)
 
     return preprocessed_image
 
@@ -27,6 +41,23 @@ def text_detection(input_image):
 
     # TODO: Preprocess the image for the text detection model
 
+    """
+    Inputs
+    name: "input" , shape: [1x3x768x1280] - An input image in the format [BxCxHxW], where:
+
+    B - batch size
+    C - number of channels
+    H - image height
+    W - image width
+    Expected color order - BGR.
+    """
+
+    height = 768
+    width = 1280
+
+    preprocessed_image = cv2.resize(input_image, (width, height))
+    preprocessed_image = preprocessed_image.transpose((2,0,1))
+    preprocessed_image = preprocessed_image.reshape(1, 3, height, width)
 
     return preprocessed_image
 
@@ -42,6 +73,27 @@ def car_meta(input_image):
 
     # TODO: Preprocess the image for the car metadata model
     
+
+    '''
+    Inputs
+    name: "input" , shape: [1x3x72x72] - An input image in following format [1xCxHxW], where:
+    - C - number of channels
+    - H - image height
+    - W - image width.
+    Expected color order - BGR.
+
+    Outputs
+    name: "color", shape: [1, 7, 1, 1] - Softmax output across seven color classes [white, gray, yellow, red, green, blue, black]
+    name: "type", shape: [1, 4, 1, 1] - Softmax output across four type classes [car, bus, truck, van]
+    '''
+
+
+    height = 72
+    width = 72
+
+    preprocessed_image = cv2.resize(input_image, (width, height))
+    preprocessed_image = preprocessed_image.transpose((2,0,1))
+    preprocessed_image = preprocessed_image.reshape(1, 3, height, width)
 
 
     return preprocessed_image
